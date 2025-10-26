a4f3a70b6133e776e95d4c3ff281d680c5415e69
MohammedSaad2701 <mohammed.24bcs10129@sst.scaler.com>
2025-10-26 15:50:27 +0530

    push

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ const app = express();
 const server = http.createServer(app);
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
